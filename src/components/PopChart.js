5278c4f5427b12c317e1d1fdd92749176a078fe4
PatrickHickey <codewithpat@gmail.com>
2018-11-25 23:51:25 -0800

    Added chart info

--- src/components/PopChart.js
+++ src/components/PopChart.js
@@ -5,8 +5,63 @@ class PopChart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      optiosn: {},
-      series: [{}]
+      optiosn: {
+        chart: {
+          background: '#f4f4f4',
+          foreColor: '#333'
+        },
+        xaxis: {
+          categories: [
+            'New York',
+            'Los Angeles',
+            'Chicago',
+            'Houston',
+            'Philadelphia',
+            'Phoenix',
+            'San Antonio',
+            'San Diego',
+            'Dallas',
+            'San Jose'
+          ]
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false
+          }
+        },
+        fill: {
+          colors: ['#f44336']
+        },
+        dataLabels: {
+          enabled: false
+        },
+        title: {
+          text: 'Largest US Cities By Population',
+          align: 'center',
+          margin: 20,
+          offsetY: 20,
+          style: {
+            fontSize: '25px'
+          }
+        }
+      },
+      series: [
+        {
+          name: 'Population',
+          data: [
+            8550405,
+            3971883,
+            2720546,
+            2296224,
+            1567442,
+            1563025,
+            1469845,
+            1394928,
+            1300092,
+            1026908
+          ]
+        }
+      ]
     };
   }
 
